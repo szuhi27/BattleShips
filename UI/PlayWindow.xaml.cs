61fcd37ba8c4b6a303bc8c6528f488688cd9c964
David <szuhid@gmail.com>
2021-11-12 16:23:06 +0100

    play window created, design started

---
+++ UI/PlayWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BattleShips.UI
+{
+    /// <summary>
+    /// Interaction logic for PlayWindow.xaml
+    /// </summary>
+    public partial class PlayWindow : Window
+    {
+        public PlayWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void PlayW_Closed(object sender, EventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            this.Visibility = Visibility.Collapsed;
+            mainWindow.Show();  
+        }
+    }
+}
